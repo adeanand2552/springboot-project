d09a327071cd70b0f5cd461c11ff7bc71cb4c514
Anand Ade <160464178+adeanand2552@users.noreply.github.com>
2024-03-21 13:14:41 +0530

    Create StartApplication.java

---
+++ springboot-app/src/main/java/com/anand/StartApplication.java
@@ -0,0 +1,24 @@
+package com.anand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Successfuly built springboot app using Maven");
+        model.addAttribute("msg", "This app is further deployed on to K8s using Argo CD");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
